c16739d719c5b3d5432f7fa0c7da82a811c56710
danish <148345298+danishsgithub@users.noreply.github.com>
2023-11-30 10:44:27 +0800

    added shopping cart feature, jump to category btn

--- javascript files/script.js
+++ javascript files/script.js
@@ -1,3 +1,32 @@
 function showMessage(message) {
     var confirmation = confirm(message);
 }
+
+function goToContainer1() {
+    document.getElementById('shop-container-1').scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  function goToContainer2() {
+    document.getElementById('shop-container-2').scrollIntoView({ behavior: 'smooth' });
+  }
+
+
+
+let cartCount = 0;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+  addToCartButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      cartCount++;
+      document.getElementById('cart-count').innerText = cartCount;
+      button.innerText = 'Added!';
+      button.style.backgroundColor = 'green';
+      setTimeout(function() {
+        button.innerText = 'Add to Cart';
+        button.style.backgroundColor = '';
+      }, 1000);
+    });
+  });
+});
